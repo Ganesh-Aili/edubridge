32e34ac1e05b1498fa3a6bd66772493308116022
Ganesh Aili <Ganesh Aili@Ganesh-Aili>
2022-09-20 15:14:08 +0530

    first commit upload all files

---
+++ SchoolManagmentSystem/src/schoolMngt/Main.java
@@ -0,0 +1,26 @@
+package schoolMngt;
+
+//import entity.Person;
+//import entity.Student;
+//import entity.Teacher;
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+	
+//		Student obj=new Student();
+//		Teacher obj1=new Teacher();
+//		show(obj);
+//		show(obj1);
+		Menu.options();
+		
+	}
+//	public static void show(Person obj) {
+//		obj.display();
+//		
+//	}
+//		
+
+}
